f63d3097326430ecc5b18fd06776855435b1e229
John Critchley <john.z80asm@critchley.biz>
2023-03-18 09:10:11 +0000

    working with polymorphism trying to duplicate a bunch of polymorphic objects the parent (a) has 2 subclasses. If either of objects from either derived classes (ab or ac) is duplicated you get an a pointer to an ab or ac as appropriate.
    Can this be done with new a(ab_object) and new a(ac_object)?

---
+++ test4c.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#include <memory>
+
+#define DEL delete
+#define LINE puts("------------")
+
+typedef class a
+{
+public:
+	a(void)
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	a(const class a & that)
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	virtual ~a()
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	virtual void print(void)
+		= 0;
+	const class a & me(void) const
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+		return *this;
+	}
+	virtual class a * another(void)
+		= 0;
+} * my_a_type;
+class ab:public a
+{
+	time_t tm;
+public:
+	void print(void)
+	{	puts("AB");
+		printf("%ld\n", tm);
+	}
+	ab(const class ab & that)
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+		tm=that.tm;
+	}
+	ab(void)
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+		tm=time(0);
+	}
+	~ab()
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	virtual class a * another(void)
+	{	return new ab(*this);
+	}
+};
+class ac:public a
+{
+public:
+	void print(void)
+	{	puts("AC");
+	}
+	ac(const class ac & that)
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	ac(void)
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	~ac()
+	{	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+	}
+	virtual class a * another(void)
+	{	return new ac();
+	}
+};
+class my
+{
+public:
+	my(void)
+	{
+		std::vector<a *> vec_a={new ab, new ac};
+		std::vector<a *> vec_b;
+		for (a * i: vec_a)
+			vec_b.push_back(i->another());
+		for (a * i: vec_a)
+			i->print();
+		LINE;
+		for (a * i: vec_b)
+			i->print();
+		LINE;
+		for (a * i: vec_a)
+			DEL i;
+		for (a * i: vec_b)
+			DEL i;
+	}
+} static my;
+int main(int argc, char * * argv)
+{
+	printf("%s %d %s\n", __PRETTY_FUNCTION__, __LINE__, __FILE__);
+}
